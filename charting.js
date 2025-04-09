3b6442e14c3fc49ac758c2a6aaf79bd190df7618
julia <julia-heinzelmann@gmx.net>
2025-04-09 22:15:17 +0200

    initial commit

---
+++ charting.js
@@ -0,0 +1,29 @@
+const canvas1 = document.getElementById('chart1');
+
+//call chart1 function on page load
+window.onload = function() {
+  chart1();
+}
+
+function chart1(){
+
+
+  new Chart(canvas1, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
